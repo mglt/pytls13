b8788bf7b18055b98adfd364ed39766b292791b1
mglt <mglt.biz@gmail.com>
2023-04-12 10:36:46 -0400

    updating conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,9 +18,9 @@ release = '0.1'
 import os
 import sys
 ##sys.path.insert( 0, os.path.abspath( '../../../pylurk.git/src/pylurk/' ))
-##sys.path.insert( 0, os.path.abspath( '../../src/pytls13/' ))
-sys.path.insert( 0, os.path.abspath( '/home/mglt/gitlab/pytls13/src/pytls13' ))
-sys.path.insert( 0, os.path.abspath( '/home/mglt/gitlab/pytls13/src' ))
+sys.path.insert( 0, os.path.abspath( '../../src/pytls13/' ))
+#sys.path.insert( 0, os.path.abspath( '/home/mglt/gitlab/pytls13/src/pytls13' ))
+#sys.path.insert( 0, os.path.abspath( '/home/mglt/gitlab/pytls13/src' ))
 #sys.path.insert( 0, os.path.abspath( '/home/mglt/gitlab/pylurk.git/src' ))
 
 
